d2ef01539a6baca5c818f5105f8655b4457efc31
Alex Zenk <azenk@css.edu>
2021-04-25 19:52:47 -0500

    Finished

--- Coins/Form1.Designer.cs
+++ Coins/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SubmitBTN = new System.Windows.Forms.Button();
+            this.IntegerTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SubmitBTN
+            // 
+            this.SubmitBTN.Location = new System.Drawing.Point(250, 73);
+            this.SubmitBTN.Name = "SubmitBTN";
+            this.SubmitBTN.Size = new System.Drawing.Size(106, 36);
+            this.SubmitBTN.TabIndex = 0;
+            this.SubmitBTN.Text = "Submit";
+            this.SubmitBTN.UseVisualStyleBackColor = true;
+            this.SubmitBTN.Click += new System.EventHandler(this.SubmitBTN_Click);
+            // 
+            // IntegerTB
+            // 
+            this.IntegerTB.Location = new System.Drawing.Point(87, 73);
+            this.IntegerTB.Name = "IntegerTB";
+            this.IntegerTB.Size = new System.Drawing.Size(136, 20);
+            this.IntegerTB.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.IntegerTB);
+            this.Controls.Add(this.SubmitBTN);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button SubmitBTN;
+        private System.Windows.Forms.TextBox IntegerTB;
     }
 }
 
--- Coins/Form1.cs
+++ Coins/Form1.cs
@@ -16,5 +16,119 @@ namespace Coins
         {
             InitializeComponent();
         }
+
+        private void SubmitBTN_Click(object sender, EventArgs e)
+        {
+            int coins = 0;
+            int coins1 = 0;
+            if(!InRange())
+            {
+                MessageBox.Show("Must enter an integer between 1 and 250", "Out of Range");
+            }
+            else
+            {
+                int value = int.Parse(IntegerTB.Text);
+
+               
+                coins  = NumberOfCoins(int.Parse(IntegerTB.Text));
+
+                coins1 = NumOfCoins(int.Parse(IntegerTB.Text));
+
+                if(coins < coins1)
+                {
+                    MessageBox.Show(IntegerTB.Text + " can be achieved with " + coins + " coins!");
+                }
+                else
+                {
+                    MessageBox.Show(IntegerTB.Text + " can be achieved with " + coins1 + " coins!");
+                }
+            }
+        }
+
+  
+        private int NumOfCoins(int value)
+        {
+            int coins = 0;
+
+            while(value > 0)
+            {
+                if(value >= 11)
+                {
+                    value -= 11;
+                }
+                else if(value >= 9)
+                {
+                    value -= 9;
+                }
+                else if(value >= 7)
+                {
+                    value -= 7;
+                }
+                else if(value >= 5)
+                {
+                    value -= 5;
+                }
+                else if(value >= 1)
+                {
+                    value -= 1;
+                }
+
+                coins++;
+
+            }
+
+            return coins;
+        }
+
+        private int NumberOfCoins(int value)
+        {
+            int coins = 0;
+
+            while(value > 0)
+            {
+                if(value >= 11)
+                {
+                    value -= 11;
+                    coins++;
+                }
+                if (value >= 9)
+                {
+                    value -= 9;
+                    coins++;
+                }
+                if (value >= 7)
+                {
+                    value -= 7;
+                    coins++;
+                }
+                if (value >= 5)
+                {
+                    value -= 5;
+                    coins++;
+                }
+                if(value >= 1)
+                {
+                    value -= 1;
+                    coins++;
+                }
+
+                
+            }
+            
+            return coins;
+        }
+
+        private bool InRange()
+        {
+            bool valid = false;
+
+            
+            if(int.Parse(IntegerTB.Text) >= 1 && int.Parse(IntegerTB.Text) <= 250)
+            {
+                valid = true;
+            }
+
+            return valid;
+        }
     }
 }
